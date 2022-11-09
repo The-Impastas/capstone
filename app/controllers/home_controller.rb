a3ef0b0baeb46e926d4628bb2d8a3d3a0cf5e813
Heath-H <heathhaupt92@gmail.com>
2022-11-09 09:52:38 -0800

    most frontend structure complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,41 @@
 class HomeController < ApplicationController
+  
+  
   def index
-  end
-end
+    pasta = Pasta.all
+    render json: pasta
+    end
+    
+    def create
+    pasta = Pasta.create(pasta_params)
+    if pasta.valid?
+    render json: pasta
+    else
+    render json: pasta.errors, status: 422
+    end
+    end
+    
+    def update
+    pasta = Pasta.find(params[:id])
+    pasta.update(pasta_params)
+    if pasta.valid?
+    render json: pasta
+    else
+    render json: pasta.errors, status: :unprocessable_entity
+    end
+    end
+    
+    def destroy
+    pasta = Pasta.find(params[:id])
+    if pasta.destroy
+    render json: pasta
+    else
+    render json: pasta.errors
+    end
+    end
+    
+    private
+    def pasta_params
+    params.require(:pasta).permit(:name, :protein, :sauce, :beverage, :cheese, recipe, :image, :user_id)
+    end
+    end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+pasta = [
+  {
+    pasta: 'Angel Hair',
+    protein: 'turkey',
+    sauce: 'Pumpkin sauce',
+    beverage: 'red sauvignon blanc',
+    cheese: 'parm',
+    recipe_link: 'google.com',
+    image: 'pasta.image.com' ,
+    user_id: 1,
+  },
+  {
+    pasta: 'Angel Hair',
+    protein: 'turkey',
+    sauce: 'Pumpkin sauce',
+    beverage: 'red sauvignon blanc',
+    cheese: 'parm',
+    recipe_link: 'google.com',
+    image: 'pasta.image.com' ,
+    user_id: 1,
+  },
+  {
+    pasta: 'Angel Hair',
+    protein: 'turkey',
+    sauce: 'Pumpkin sauce',
+    beverage: 'red sauvignon blanc',
+    cheese: 'parm',
+    recipe_link: 'google.com',
+    image: 'pasta.image.com' ,
+    user_id: 1,
+  }
+]
+
+cats.each do |each_cat|
+  Cat.create each_cat
+  puts "creating cat #{each_cat}"
+end
