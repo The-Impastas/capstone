60e40f6232d41dcc67c53cbd7ca108fd42d19c15
Heath-H <heathhaupt92@gmail.com>
2022-11-09 08:27:23 -0800

    most frontend structure complete

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App
