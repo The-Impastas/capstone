e77022175d72d94fa54f82e4df15757b80b10605
Heath-H <heathhaupt92@gmail.com>
2022-11-09 13:02:43 -0800

    frontend structure completed again

--- app/controllers/pasta_controller.rb
+++ app/controllers/pasta_controller.rb
@@ -1,2 +1,43 @@
 class PastaController < ApplicationController
+
+    def index
+        pasta = Pasta.all
+        render json: pasta
+        end
+        
+        def create
+        pasta = Pasta.create(pasta_params)
+        if pasta.valid?
+        render json: pasta
+        else
+        render json: pasta.errors, status: 422
+        end
+        end
+        
+        def update
+        pasta = Pasta.find(params[:id])
+        pasta.update(pasta_params)
+        if pasta.valid?
+        render json: pasta
+        else
+        render json: pasta.errors, status: :unprocessable_entity
+        end
+        end
+        
+        def destroy
+        pasta = Pasta.find(params[:id])
+        if pasta.destroy
+        render json: pasta
+        else
+        render json: pasta.errors
+        end
+        end
+        
+        private
+        def pasta_params
+        params.require(:pasta).permit(:name_of_pasta, :protein, :sauce, :beverage, :cheese, :recipe_link, :image, :user_id)
+        end
+        end
+
+
 end
