04f26ed80f04421572c97b4ee26856df26af4aea
TheRealRion <112355675+TheRealRion@users.noreply.github.com>
2022-11-15 15:52:13 -0800

    ADDED WORKING NAV BAR WITH SEARCH FUNCTION

--- app/javascript/components/components/Navigation.js
+++ app/javascript/components/components/Navigation.js
@@ -9,43 +9,90 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import SearchIcon from '@mui/icons-material/Search'
-
-
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import InputBase from '@mui/material/InputBase'
+import { styled, alpha } from '@mui/material/styles';
+import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
+import Link from "@mui/material/Link";
 
 const  Navigation = ({
   logged_in,
-  current_user,
-  new_user_route,
-  sign_in_route,
-  sign_out_route
-})  => {
+  })  => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
+    const pages = [   
+      <Link className="link" href="/index">
+      Pastas
+      </Link>,
+      <Link className="link" href="/users/sign_in">
+      Sign In
+      </Link>,
+      <Link className="link" href="/users/sign_out">
+      Sign Out
+      </Link>,
+      <Link className="link" href="/users/sign_up">
+      Sign Up
+      </Link>,
+      <Link className="link" href="/aboutus">
+      About Us
+      </Link>, ];
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    // search bar stuff //
+    const Search = styled('div')(({ theme }) => ({
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    }));
+
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }));
+
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+      color: 'inherit',
+      '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: '12ch',
+          '&:focus': {
+            width: '20ch',
+          },
+        },
+      },
+    }));
+    // search bar stuff end //
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+    <AppBar color="warning" position="fixed">
+      <Container  maxWidth="xl">
+        <Toolbar  disableGutters>
+          <DinnerDiningIcon  sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -61,7 +108,7 @@ const  Navigation = ({
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Pasta Pairings
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -94,13 +141,13 @@ const  Navigation = ({
               }}
             >
               {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
+                <MenuItem  key={page} onClick={handleCloseNavMenu}>
+                  <Typography  textAlign="center">{page}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <DinnerDiningIcon  sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
@@ -117,7 +164,7 @@ const  Navigation = ({
               textDecoration: 'none',
             }}
           >
-            LOGO
+            PP
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
@@ -130,7 +177,15 @@ const  Navigation = ({
               </Button>
             ))}
           </Box>
-
+              <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
           <Box sx={{ flexGrow: 0 }}>
             
           </Box>
