7ab8d4ce77f9055b998a73bee7e0e56cdf880dd1
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-11-16 13:24:25 -0800

    added footer

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -1,19 +1,23 @@
-import React from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import { Typography } from '@mui/material';
+
 
 const Footer = () => {
 
-    return (
-        <>
-          <div className="footer" > 
-          
-          <>PASTA</> 
-          
-          </div> 
-          <br/>
-        </>
-    )
-    
-    
-    }
-    
-    export default Footer
+  const [value, setValue] = React.useState(0);
+  const ref = React.useRef(null);
+
+  return (
+    <Box sx={{ pb: 7 }} ref={ref}>
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <Typography textAlign="center">
+          Lovingly Created by The Impastas, LLC 2022 ©
+        </Typography>
+      </Paper>
+    </Box>
+  );
+}
+
+export default Footer
