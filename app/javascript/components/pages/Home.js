f588ff519bb417098892bad7a035523a753b5167
nathaniel-ross <nathaniel.rossloyo@gmail.com>
2022-11-16 13:54:03 -0800

    working image with button

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,43 +1,123 @@
 import React from "react";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 const Home = () => {
 
-  return (
+const images = [
+  {
+    url: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/10/7/0/FNK_Angel-Hair-with-Pesto_s4x3.jpg.rend.hgtvcom.616.462.suffix/1383814629222.jpeg',
+    title: 'View Pastas',
+    width: '60%',
+  },
+];
 
-    <div className="home">
-      {/* breaks push down text out from under nav bar, fix with margin?  */}
-      <br />
-      <br />
-      <br />
-      <>pasta pasta yum</>
-      <body className="home">
-        <br />
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <br />
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-        <div>find your perfect pasta, or create a new recipe. share with your friends and family, to bring one another together over a beautifly crafted meal</div>
-      </body>
-    </div>
-  )
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: 'relative',
+  height: 200,
+  [theme.breakpoints.down('sm')]: {
+    width: '100% !important', // Overrides inline-style
+    height: 100,
+  },
+  '&:hover, &.Mui-focusVisible': {
+    zIndex: 1,
+    '& .MuiImageBackdrop-root': {
+      opacity: 0.15,
+    },
+    '& .MuiImageMarked-root': {
+      opacity: 0,
+    },
+    '& .MuiTypography-root': {
+      border: '4px solid currentColor',
+    },
+  },
+}));
+
+const ImageSrc = styled('span')({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center 40%',
+});
+
+const Image = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create('opacity'),
+}));
+
+const ImageMarked = styled('span')(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: 'absolute',
+  bottom: -2,
+  left: 'calc(50% - 9px)',
+  transition: theme.transitions.create('opacity'),
+}));
+
+  return (
+    <Container
+    sx={{
+      mt: 10,
+    }}>
+      {/* Box starts the image button box */}
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%', justifyContent: 'center', top: 100}}>
+      {images.map((image) => (
+        <ImageButton
+          focusRipple
+          key={image.title}
+          style={{
+            width: image.width,
+          }}
+        >
+          <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+          <ImageBackdrop className="MuiImageBackdrop-root" />
+          <Image>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              sx={{
+                position: 'relative',
+                p: 4,
+                pt: 2,
+                pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+              }}
+            >
+              {image.title}
+              <ImageMarked className="MuiImageMarked-root" />
+            </Typography>
+          </Image>
+        </ImageButton>
+      ))}
+    </Box>
+    </Container>
+  );
 }
+
 
 export default Home
