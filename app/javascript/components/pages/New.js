01a40e71ccbae8422915911e957d0f25e1f750ab
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-11-17 14:47:10 -0800

    fixed indentations

--- app/javascript/components/pages/New.js
+++ app/javascript/components/pages/New.js
@@ -27,35 +27,71 @@ const New = ({ createPasta, current_user }) => {
     <Paper>
       <h1>Fill in the form to Create your pasta!</h1>
       <br />
-      <TextField required id="outlined-required" label="Name of Pasta" name="name_of_pasta" onChange={handleChange} value={newPasta.name_of_pasta}>
+      <TextField 
+      required id="outlined-required" 
+      label="Name of Pasta" 
+      name="name_of_pasta" 
+      onChange={handleChange} 
+      value={newPasta.name_of_pasta}>
       </TextField>
       <br />
       <br />
-      <TextField required id="outlined-required" label="Name of Protein" name="protein" onChange={handleChange} value={newPasta.protein}>
+      <TextField 
+      required id="outlined-required" 
+      label="Name of Protein" 
+      name="protein" 
+      onChange={handleChange} 
+      value={newPasta.protein}>
       </TextField>
       <br />
       <br />
-      <TextField required id="outlined-required" label="Name of Sauce" name="sauce" onChange={handleChange} value={newPasta.sauce}>
+      <TextField 
+      required id="outlined-required" 
+      label="Name of Sauce" 
+      name="sauce" 
+      onChange={handleChange} 
+      value={newPasta.sauce}>
       </TextField>
       <br />
       <br />
-      <TextField required id="outlined-required" label="Name of Beverage" name="beverage" onChange={handleChange} value={newPasta.beverage}>
+      <TextField 
+      required id="outlined-required" 
+      label="Name of Beverage" 
+      name="beverage" 
+      onChange={handleChange} 
+      value={newPasta.beverage}>
       </TextField>
       <br />
       <br />
-      <TextField required id="outlined-required" label="Name of Cheese" name="cheese" onChange={handleChange} value={newPasta.cheese}>
+      <TextField 
+      required id="outlined-required" 
+      label="Name of Cheese" 
+      name="cheese" 
+      onChange={handleChange} 
+      value={newPasta.cheese}>
       </TextField>
       <br />
       <br />
-      <TextField required id="outlined-required" label="Link to Recipe" name="recipe_link" onChange={handleChange} value={newPasta.recipe_link}>
+      <TextField 
+      required id="outlined-required" 
+      label="Link to Recipe" 
+      name="recipe_link" 
+      onChange={handleChange} 
+      value={newPasta.recipe_link}>
       </TextField>
       <br />
       <br />
-      <TextField required id="outlined-required" label="Image Link" name="image" onChange={handleChange} value={newPasta.image}>
+      <TextField 
+      required id="outlined-required" 
+      label="Image Link" 
+      name="image" 
+      onChange={handleChange} 
+      value={newPasta.image}>
       </TextField>
       <br />
       <br />
-      <Button onClick={handleSubmit} name="submit">
+      <Button onClick={handleSubmit} 
+              name="submit">
         Submit Pasta
       </Button>
     </Paper>
