276a10e165a8d7109695894643bfb7b7acbd6c02
TheRealRion <112355675+TheRealRion@users.noreply.github.com>
2022-11-21 10:41:12 -0800

    added rating to index cards

--- app/javascript/components/pages/Index.js
+++ app/javascript/components/pages/Index.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -8,8 +8,38 @@ import Typography from '@mui/material/Typography';
 import { NavLink } from "react-router-dom"
 import Link from '@mui/material'
 import {Container, Grid} from '@mui/material'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useState } from 'react';
+
+
 
 const Index = ({pastas}) => {
+  const StyledRating = styled(Rating)({
+    '& .MuiRating-iconFilled': {
+      color: '#ff6d75',
+    },
+    '& .MuiRating-iconHover': {
+      color: '#ff3d47',
+    },
+  });
+
+  const [newRating, setNewRating] = useState({
+    userRating: ""
+  })
+
+  const handleChange = (e) => {
+    setNewRating({ ...newRating, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = () => {
+    addRating(newRating)
+    
+  }
+
+
     return (
       <Container
       sx={{
@@ -41,6 +71,25 @@ const Index = ({pastas}) => {
                             <Typography align="center" variant="h5" component="div">
                               <NavLink to={`/show/${pasta.id}`}>{pasta.name_of_pasta}</NavLink>
                             </Typography>
+                            <br/>
+                            <Typography component="legend">Rate this Recipe</Typography>
+                            <br/>
+                            <StyledRating
+                            align="center"
+                            name="customized-color"
+                            defaultValue={2}
+                            getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
+                            precision={0.5}
+                            icon={<FavoriteIcon fontSize="inherit" />}
+                            emptyIcon={<FavoriteBorderIcon fontSize="inherit" 
+                            onChange ={handleChange}/>}
+                            />
+                            <br/>
+                            <Button onClick={handleSubmit} 
+                              name="submit"
+                              align="center">
+                              Submit Rating
+                            </Button>
                           </CardContent>
                         </Card>
                     <br />
