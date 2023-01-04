549d609f843249e57970b37264a39740a34d61eb
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2023-01-04 13:33:31 -0800

    render ready?

--- app/models/pasta.rb
+++ app/models/pasta.rb
@@ -2,6 +2,4 @@ class Pasta < ApplicationRecord
     belongs_to :user
     validates :name_of_pasta, :protein, :sauce, :beverage, :cheese, :recipe_link, :image, presence: true
     validates_presence_of :name_of_pasta, :protein, :sauce, :beverage, :cheese, :recipe_link, :image, :message  => "Please fill out all text boxes"
-    
-    # validates_format_of :name_of_pasta, :with => /\A[a-z]+\z/i
 end
